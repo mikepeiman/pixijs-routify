0614d81ec757e85e5de537aa46f0c42f91ba28bb
Mike Peiman <mikepeiman@gmail.com>
2020-06-23 13:10:42 -0400

    Distributing subsequent generatedShips around circle. Still not getting animation speed adjusted

--- src/pages/example/pixi/_fallback.svelte
+++ src/pages/example/pixi/_fallback.svelte
@@ -3,7 +3,17 @@
   import * as PIXI from "pixi.js";
   import { circular } from "./circular.js";
   //  using video tutorial at https://www.youtube.com/watch?v=2J0VUiozAVM&list=PL08jItIqOb2oGcyrgREbrm_b9OW7TE1ji&index=3
-
+  let ships = [],
+    radialInterval,
+    numShips;
+  $: {
+    numShips = ships.length || 1;
+    console.log(`numShips reactive update ${numShips}`);
+  }
+  $: {
+    radialInterval = 360 / numShips;
+    console.log(`radialInterval ${radialInterval}`);
+  }
   onMount(() => {
     let type = "WebGL";
     if (!PIXI.utils.isWebGLSupported()) {
@@ -20,32 +30,41 @@
 
     // initialize container for ships
     let container = new PIXI.Container();
-    // container.position.x = 0 - app.renderer.screen.width / 2;
-    // container.position.y = 0 - app.renderer.screen.height / 2;
-    // container.width = app.renderer.screen.width
-    // container.height = app.renderer.screen.height
     let rw = app.renderer.width;
     let rh = app.renderer.height;
     container.position = new PIXI.Point(0, 0);
-    // container.x = rw / 2;
-    // container.x = rw / 2;
-    // container.y = 0;
-    // container.y = 0;
     container.width = rw;
     container.height = rh;
     app.stage.addChild(container);
     let delta = 0;
     let _w = window.innerWidth;
     let _h = window.innerHeight;
-
+    let w = _w / 2;
+    let h = _h / 2;
+    let angle = 0;
     // generate array of ships Graphics() objects
-    let ships = [];
-    generateShips(50);
 
-    app.ticker.deltaMS = 1000;
-    app.ticker.deltaTime = 0.1;
-    app.ticker.add(animate);
-    app.ticker.start();
+    generateShips(30);
+    setTimeout(() => {
+      generateShips(30);
+      // setTimeout(() => {
+      //   generateShips(30);
+      //   setTimeout(() => {
+      //     generateShips(30);
+      //     setTimeout(() => {
+      //       generateShips(30);
+      //       setTimeout(() => {
+      //         generateShips(30);
+      //       }, 500);
+      //     }, 500);
+      //   }, 500);
+      // }, 500);
+    }, 1500);
+
+    // app.ticker.deltaMS = 1000000;
+    // app.ticker.deltaTime = 0.001;
+    // app.ticker.add(animate);
+    // app.ticker.start();
 
     PIXI.utils.sayHello(type);
     console.log(PIXI.utils.TextureCache);
@@ -58,24 +77,6 @@
       app.renderer.resize(_w, _h);
     }
 
-    // let starsGroup = new PIXI.Container();
-    // app.stage.addChild(starsGroup);
-
-    // app.stage.addChild(ship)
-    function circular(element, windowWidth, windowHeight, x, y, angle) {
-      let w = windowWidth;
-      let h = windowHeight;
-      let el = element;
-      console.log(`circular.js imported not IIFE`);
-      x = w + w * Math.cos((angle * Math.PI) / 180);
-      y = h + h * Math.sin((angle * Math.PI) / 180);
-      angle++;
-      angle > 360 ? angle : (angle = 0);
-      setTimeout(() => {
-        circular(el, w, h, x, y, angle);
-      }, 20);
-    }
-    let angle = 0;
     let shipC = new PIXI.Graphics();
     shipC.lineStyle(3, 0xafb0ff);
     shipC.beginFill(0xafbbff);
@@ -84,38 +85,20 @@
     // shipC.position = new PIXI.point(0,0)
     container.addChild(shipC);
 
-    function makeShipCircle(el, w, h) {
-      // el = shipC
-      // let w = container.width
-      // let h = container.height
-      // w = 300
-      // h = 300
-      // w = parseInt(w)
-      // h = parseInt(h)
-      shipC.x = w + w * Math.cos((angle * Math.PI) / 180);
-      shipC.y = h + h * Math.sin((angle * Math.PI) / 180);
+    function makeShipCircle(el, w, h, i) {
+      el.x = w + w * Math.cos(((angle + radialInterval * i) * Math.PI) / 180);
+      el.y = h + h * Math.sin(((angle + radialInterval * i) * Math.PI) / 180);
       angle++;
-      // console.log(`makeShipCircle: w ${w} h ${h} angle ${angle}, shipX ${shipC.x}, shipY ${shipC.y}`)
       if (angle > 360) {
         angle = 0;
       }
       // angle > 360 ? angle : (angle = 0);
-      setTimeout(() => {
-        makeShipCircle(el, w, h);
-      }, 16);
+      // setTimeout(() => {
+      //   makeShipCircle(el, w, h, i);
+      // }, 16);
     }
-    let w = _w / 2;
-    let h = _h / 2;
-    makeShipCircle(shipC, w, h);
 
-    function nextCircle(el, w, h) {
-      el.x = w + w * Math.cos((angle * Math.PI) / 180);
-      el.y = h + h * Math.sin((angle * Math.PI) / 180);
-      angle++;
-      if (angle > 360) {
-        angle = 0;
-      }
-    }
+    makeShipCircle(shipC, w, h, 1);
 
     function generateShips(fleetSize) {
       for (let i = 0; i < fleetSize; i++) {
@@ -129,29 +112,48 @@
         let x = 0,
           y = 0,
           angle = 0;
-        // circular(ship, _w, _h, x, y, angle);
-        ship.x = Math.random() * app.renderer.screen.width;
-        ship.y = Math.random() * app.renderer.screen.height;
+        // ship.x = Math.random() * app.renderer.screen.width;
+        // ship.y = Math.random() * app.renderer.screen.height;
         ship.tint = Math.random() * 0xffffff;
         // ship.anchor.set(0.5);
         // ship.blendMode = PIXI.BLEND_MODES.MULTIPLY
         container.addChild(ship);
-        ships.push(ship);
+        // ships.push(ship);
+        ships = [...ships, ship];
+        makeShipCircle(ship, w, h, i);
       }
     }
+    let i = 0, j = 0
+    function draw(el) {
+      // requestAnimationFrame(draw(el));
+      // el.fillStyle = "rgba(255, 255, 255, 0.4)";
+      // el.fillRect(0, 0, canvas.width, canvas.height);
+
+      // mouseOld.x += (mouse.x - mouseOld.x) * 0.05;
+      // mouseOld.y += (mouse.y - mouseOld.y) * 0.05;
+      let x = w + Math.cos(j) * 100;
+      let y = h + Math.sin(j) * 100;
+
+      let xx = x + Math.cos(i + j) * 50;
+      let yy = y + Math.sin(i + j) * 50;
+      el.x = xx
+      el.y = yy
+      i += 0.08;
+      j += 0.05;
+    }
+
+    draw(shipC);
 
     function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    let deltaX, deltaY;
-    console.log(`container (ships) children `, container.children);
-    // let delta = 0;
     function animate() {
-      deltaX = 0.01;
-      for (let i = 0; i < ships.length; i++) {
-        nextCircle(ships[i], w, h);
-      }
+      // requestAnimationFrame(animate)
+      // for (let i = 0; i < ships.length; i++) {
+      //   makeShipCircle(ships[i], w, h, i);
+      // }
+      // app.renderer.render(app.stage)
     }
   });
 </script>

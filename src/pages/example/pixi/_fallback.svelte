a466826c1d575140e6c87cebb536ffbd8b9ec660
Mike Peiman <mikepeiman@gmail.com>
2020-06-24 09:15:37 -0400

    Refactored star and ships creation, animation now inside ticker animate function, ships placed correctly - number of ship incorrect

--- src/pages/example/pixi/_fallback.svelte
+++ src/pages/example/pixi/_fallback.svelte
@@ -5,17 +5,18 @@
   //  using video tutorial at https://www.youtube.com/watch?v=2J0VUiozAVM&list=PL08jItIqOb2oGcyrgREbrm_b9OW7TE1ji&index=3
   let ships = [],
     stars = [],
-    radialInterval,
-    numShips,
+    // radialInterval,
+    // numShips,
+    globalCount = 0,
     rotationSpeed = 0.0001;
-  $: {
-    numShips = ships.length || 30;
-    console.log(`numShips reactive update ${numShips}`);
-  }
-  $: {
-    radialInterval = 360 / numShips;
-    console.log(`radialInterval ${radialInterval}`);
-  }
+  // $: {
+  //   numShips = ships.length || 30;
+  //   console.log(`numShips reactive update ${numShips}`);
+  // }
+  // $: {
+  //   radialInterval = 360 / numShips;
+  //   console.log(`radialInterval ${radialInterval}`);
+  // }
   onMount(() => {
     let type = "WebGL";
     if (!PIXI.utils.isWebGLSupported()) {
@@ -49,8 +50,8 @@
 
     // app.ticker.deltaMS = 1000000;
     // app.ticker.deltaTime = 0.001;
-    // app.ticker.add(animate);
-    // app.ticker.start();
+    app.ticker.add(animate);
+    app.ticker.start();
 
     PIXI.utils.sayHello(type);
     console.log(PIXI.utils.TextureCache);
@@ -63,56 +64,55 @@
       app.renderer.resize(_w, _h);
     }
 
-    placeShipsAroundStar(origin, radius, _w, _h, 20);
+    createStarAndShips(origin, radius, 20);
     radius = 75;
     origin = { x: w - radius * 2, y: h + radius * 2 };
-    placeShipsAroundStar(origin, radius, _w, _h, 20);
+    createStarAndShips(origin, radius, 12);
     radius = 35;
     origin = { x: w + radius * 2, y: h - radius * 2 };
-    placeShipsAroundStar(origin, radius, _w, _h, 20);
+    createStarAndShips(origin, radius, 7);
 
-    function createStarContainer(origin) {
-      console.log(`createStarContainer(origin) ${origin.x}:${origin.y}`);
-      let container = new PIXI.Container();
-      // mainContainer.addChild(container);
-      container.position = new PIXI.Point(origin.x, origin.y);
-      // app.stage.addChild(container);
-      return container;
+    function createStarAndShips(origin, radius, numShips) {
+      let container = createStar(radius, origin);
+      addShipsToStar1(container, numShips);
     }
 
     function createStar(radius, origin) {
       let container = new PIXI.Container();
       let star = new PIXI.Graphics();
       let numStars = stars.length;
-      console.log(`number of stars: ${numStars}`);
+      container.id = numStars;
+      console.log(
+        `number of stars: ${numStars}, star.id ${star.id}, container.id ${container.id}`
+      );
       star.position = new PIXI.Point(0.5, 0.5);
-      star.starNum = numStars + 1;
+      star.id = numStars;
+      star.origin = origin;
+      star.radius = radius;
       star.lineStyle(1, 0x0fb0ff);
-      star.beginFill(0xaabbff);
+      star.beginFill(Math.random() * 0xffffff);
       star.drawCircle(0, 0, radius - 15);
       star.endFill();
       star.tint = 0xffffff;
       star.x = container.x;
       star.y = container.y;
-      star.origin = origin;
+
       // console.log('container: ',container)
       container.addChild(star);
       stars = [...stars, star];
       return container;
     }
 
-    async function placeShipsAroundStar(origin, radius, w, h, numShips) {
-      let container = createStar(radius, origin);
+    async function addShipsToStar1(container, numShips) {
       container.position = new PIXI.Point(origin.x, origin.y);
       mainContainer.addChild(container);
-
       let increase = (Math.PI * 2) / numShips;
-      for (let z = 0; z < stars.length; z++) {
-        console.log(`separate stars loop, star ${z}`);
-      }
-      // work for tomorrow: create ships for each star as array in that star object
-      // add animation loop as distinct function for each star-ships array
-      for (let j = 0; j < numShips; j++) {
+      let id = container.id;
+      console.log(`addShipsToStar1 container.id ${container.id}`);
+      let star = stars[id];
+      ships = [];
+      star.numShips = numShips;
+      for (let i = 0; i < numShips; i++) {
         let ship = new PIXI.Graphics();
         ship.lineStyle(1, 0x0fb0ff);
         ship.beginFill(0xaabbff);
@@ -125,75 +125,47 @@
         ships = [...ships, ship];
         angle += increase;
       }
-      ships;
-      makeShipCircle(radius, origin, ships, increase);
+      star.ships = ships;
     }
 
-    function makeShipCircle(radius, origin, ships, increase) {
-      let ship;
-      // await numShips;
-      // let increase = (Math.PI * 2) / numShips;
-      for (let i = 0; i < ships.length; i++) {
-        ship = ships[i];
-        // console.log(`inside of animate ships loop #${i}/${numShips}`);
-        ship.x = origin.x + radius * Math.cos(angle);
-        ship.y = origin.y + radius * Math.sin(angle);
-        angle += increase + rotationSpeed;
-      }
-      setTimeout(() => {
-        makeShipCircle(radius, origin, ships, increase);
-      }, 16);
-    }
-
-    function generateShips(fleetSize) {
-      for (let i = 0; i < fleetSize; i++) {
-        let ship = new PIXI.Graphics();
-        ship.lineStyle(1, 0x0fb0ff);
-        ship.beginFill(0xaabbff);
-        ship.drawCircle(0, 0, 5);
-        ship.endFill();
-        let rand = Math.random() * 0.1;
-        // ship.scale = new PIXI.Point(5, 5);
-        let x = 0,
-          y = 0,
-          angle = 0;
-        // ship.x = Math.random() * app.renderer.screen.width;
-        // ship.y = Math.random() * app.renderer.screen.height;
-        ship.tint = Math.random() * 0xffffff;
-        // ship.anchor.set(0.5);
-        // ship.blendMode = PIXI.BLEND_MODES.MULTIPLY
-        mainContainer.addChild(ship);
-        // ships.push(ship);
-        ships = [...ships, ship];
-        makeShipCircle(ship, w, h, i);
+    function makeShipsCircle() {
+      let ship, star;
+      let increase = (Math.PI * 2) / ships.length;
+      for (let z = 0; z < stars.length; z++) {
+        star = stars[z];
+        origin = star.origin;
+        radius = star.radius;
+        // console.log(
+        //   `makeShipsCircle() stars loop - star id ${star.id}, ships array length ${star.ships.length}, numShips value ${star.numShips}`
+        // );
+        // console.log(
+        //   `makeShipsCircle() stars loop - star x ${star.x}, y ${star.y}, radius ${star.radius} `
+        // );
+        //         console.log(
+        //   `makeShipsCircle() stars loop - star ORIGIN x ${star.origin.x}, y ${star.origin.y}, radius ${star.radius} `
+        // );
+        for (let i = 0; i < star.numShips; i++) {
+          ship = star.ships[i];
+          // console.log(`This ship x and y: ${ship.x}, ${ship.y}`);
+          ship.x = star.origin.x + star.radius * Math.cos(angle);
+          ship.y = star.origin.y + star.radius * Math.sin(angle);
+          angle += increase + rotationSpeed;
+        }
       }
     }
-    let i = 0,
-      j = 0;
-    function draw(el) {
-      // requestAnimationFrame(draw(el));
-
-      let x = w + Math.cos(j) * 100;
-      let y = h + Math.sin(j) * 100;
-
-      let xx = x + Math.cos(i + j) * 50;
-      let yy = y + Math.sin(i + j) * 50;
-      el.x = xx;
-      el.y = yy;
-      i += 0.08;
-      j += 0.05;
-    }
-
-    // draw(shipC);
-
     function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     function animate() {
+      globalCount++;
+      console.log(`GLOBAL COUNT ${globalCount} `);
+      makeShipsCircle();
       // requestAnimationFrame(animate)
-      // for (let i = 0; i < ships.length; i++) {
-      //   makeShipCircle(ships[i], w, h, i);
+      // for (let z = 0; z < stars.length; z++) {
+      //   let star = stars[z]
+      //   console.log(`GLOBAL COUNT ${globalCount} animate function, star ${z}, id ${star.id}, ships ${star.ships.length} numShips ${star.numShips}`);
+      //   makeShipsCircle(star)
       // }
       // app.renderer.render(app.stage)
     }
